7b0447bf575dc220005206d451a603f262168885
hey-virender <bikuchauhan786+github@gmail.com>
2023-09-24 20:54:33 +0530

    minor fix and added js to enable searchbar

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-function search_homeorders(){
-    let input = document.getElementById('searchbar').value 
-    input=iput.toLowerCase();
-    let x =document.getElementsByClassName('homeorders');
-    for (i = 0;i<x.length;i++){
-        if(!x[i].innerHTML.toLowerCase().includes(input)){
-            x[i].style.display="none";
-        }
-        else {
-            x[i].style.display="list-item"
-        }
+
+function getpage(){
+    let search = document.querySelector('#searchinput').value; 
+
+    
+    alert(search);
+    if (search=="grocery"){
+        window.open("grocery.html",self);
+        
     }
+    else if(search=="cosmetic"){
+        window.open("cosmetic.html",self);
+    }
+    else if(search=="electronics"){
+        window.open("electronics.html",self);
+    }
+    else if(search=="furniture"){
+        window.open("furniture.html",self);
+    }
+    else if(search=="home appliances"){
+        window.open("home-appliances.html",self);
+    }
+    else if(search=="fashion"){
+        window.open("fashion.html",self);
+    }
+    else{
+        alert("There is nothing to search")
+    }
+    
 }
