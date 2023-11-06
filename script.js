bbe69c6b18e850bf05ef35518fd0e5100045ae37
Nikhil Ramketa <ramketa123@gmail.com>
2023-11-06 11:23:24 +0530

    css updated

--- script.js
+++ script.js
@@ -1,93 +1,29 @@
-
-// searchbar functionality added by virender chauhan
-// function getpage(){
-//     let s = document.querySelector('.searchsection').value; 
-//     let search=s.toLowerCase();
-//     if (search=="grocery"||search=="groceries"){
-//         window.open("grocery.html",self)    
-//     }
-//     else if(search=="cosmetic"||search=="comsetics"){
-//         window.open("cosmetic.html",self);
-//     }
-//     else if(search=="electronics"){
-//         window.open("electronics.html",self);
-//     }
-//     else if(search=="furniture"||search=="furnitures"){
-//         window.open("furniture.html",self);
-//     }
-//     else if(search=="home appliances"|| search=="homeappliances"||search=="homeappliance"||search=="home appliance"){
-//         window.open("home-appliances.html",self);
-//     }
-//     else if(search=="fashion"){
-//         window.open("fashion.html",self);
-//     }
-//     else if(search=="atta"||search=="flour"){
-//         window.open("Product/atta.html",self);
-//     }
-//     else if(search=="bed"){
-//         window.open("Product/atta.html",self);
-//     }
-//     else if(search=="foundation"){
-//         window.open("Product/atta.html",self);
-//     }
-//     else if(search=="fridge" || search == "refridgerator"){
-//         window.open("Product/atta.html",self);
-//     }
-//     else if(search=="grains" || search == "grain" || search == "daal"){
-//         window.open("Product/atta.html",self);
-//     }
-//     else if(search=="headphone" || search == "headphones"){
-//         window.open("Product/atta.html",self);
-//     }
-//     else if(search=="kajal" || search == "eyeliner"){
-//         window.open("Product/atta.html",self);
-//     }
-//     else if(search=="kids fashion"||search=="kids"||search=="kid"){
-//         window.open("Product/atta.html",self);
-//     }
-//     else if(search=="lipstick"||search=="lipsticks"){
-//         window.open("Product/atta.html",self);
-//     }
-//     else if(search=="men"||search=="men fashion"||search=="man"){
-//         window.open("Product/atta.html",self);
-//     }
-//     else if(search=="oil"){
-//         window.open("Product/oil.html",self);
-//     }
-//     else if(search=="phone"||search=="smartphone"){
-//         window.open("Product/atta.html",self);
-//     }
-//     else if(search=="sofa" || search === "sofaset" || search == "sofa set"){
-//         window.open("Product/atta.html",self);
-//     }
-//     else if(search=="table"){
-//         window.open("Product/atta.html",self);
-//     }
-//     else if(search=="tv" || search == "television"){
-//         window.open("Product/atta.html",self);
-//     }
-//     else if(search=="washing machine"){
-//         window.open("Product/atta.html",self);
-//     }
-//     else if(search=="watch" || search=="smartwatch"){
-//         window.open("Product/atta.html",self);
-//     }
-//     else if(search=="women fashion"||search=="women"||search=="woman"){
-//         window.open("Product/atta.html",self);
-//     }
-//     else{
-//         alert("Oops, Invalid search or product not available. More products will be added soon.");
-//     }
-
-
-//new and shorter code for searchbar
-
-
-
-
-
-// }
-// next js goals
-// better user interaction
-// client side validations
-// cart functionality like add or remove item quantity of item
+const products = [
+    { id: 1, name: "Product 1" },
+    { id: 2, name: "Product 2" },
+    { id: 3, name: "Product 3" },
+    // Add more product data here
+  ];
+  
+  const searchInput = document.getElementById("searchInput");
+  const productList = document.getElementById("productList");
+  
+  searchInput.addEventListener("input", () => {
+    const searchValue = searchInput.value.toLowerCase();
+    const results = products.filter(product => product.name.toLowerCase().includes(searchValue));
+    displayResults(results);
+  });
+  
+  function displayResults(results) {
+    productList.innerHTML = "";
+  
+    if (results.length === 0) {
+      productList.innerHTML = "<li>No products found</li>";
+    } else {
+      results.forEach(product => {
+        const listItem = document.createElement("li");
+        listItem.textContent = product.name;
+        productList.appendChild(listItem);
+      });
+    }
+  }
